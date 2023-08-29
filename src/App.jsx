d297f0ecbd1aa840a2e4fd1f50fde94d2efc52ac
Benas <benas.sarachovas@gmail.com>
2023-08-29 11:43:23 +0100

    adding routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,35 @@ import ProjectPage from './Components/ProjectPage'
 import TechnologyUsePage from './Components/TechnologyUsePage'
 import ContactMePage from './Components/ContactMePage'
 import Footer from './Components/Footer'
+import {Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
 
 function App() {
+
+  const [path, setPath] = useState(true)
+
+  const CheckPathProjects = (path) =>{
+    path == "/projects" || path == "/contact" ? setPath(false)
+    : setPath(true)
+    
+    path ? console.log(path,"the user is currently in the homepage, use autoscroll") : console.log(path,"The user is the dedicated component page, render the component alone")
+  }
+
   return(
     <div>
-      <Header/>
-      <LandingPage/>
-      <ProjectPage/>
-      <TechnologyUsePage/>
-      <ContactMePage/>
-      <Footer/>
+      <Header onClick={CheckPathProjects}/>
+      <Routes>
+        <Route path='/projects' element={<ProjectPage/>}/>
+        <Route path='/contact' element={<ContactMePage/>}/>
+      </Routes>
+
+      {path ?
+      (<>
+      <LandingPage/><ProjectPage/><TechnologyUsePage/><Footer/>
+      </>)
+      : ""
+      }
+      
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,14 @@
 import '../Styles/Header.css'
-export default function Header() {
+import {Link} from 'react-router-dom'
+export default function Header({onClick}) {
   return (
     <div className="header-container">
-        <div>Instagram</div>
-        <div>LinkedIn</div>
-        <div>Projects</div>
-        <div>Contact</div>
+      <nav>
+          <a href='https://www.instagram.com' target='_blank' rel="noopener noreferrer">Instagram</a>
+          <a href='https://www.linkedin.com' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+          <Link to='/projects' onClick={onClick("/projects")}>Projects</Link>
+          <Link to='/contact' onClick={onClick("/contact")}>Contact</Link>
+        </nav>
     </div>
   )
 }
