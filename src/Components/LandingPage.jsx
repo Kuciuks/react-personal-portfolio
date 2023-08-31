0a78baaa04e0b7bb3a163bdf5c9b04430ae1bba8
Benas <benas.sarachovas@gmail.com>
2023-09-01 00:11:00 +0100

    returning to the previous version of the code

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,13 +1,24 @@
 import '../Styles/LandingPage.css'
+
 export default function LandingPage() {
-  return (
-    <div className='landing-container'>
-      <h1 className='h1-greet'>{"Hi\nThere"}
-          <span className='span-exclamation'>!
-          </span>
-      </h1>
-      <h2 className='h2-greet'>Welcome to my portfolio page, my name is Benas and I am a front-end web developer</h2>
-      <div className='line-break'></div>
-    </div>
-  )
-}
+
+    return (
+      <div className='landing-container'>
+        <div className='welcome-div'>
+          <span className='span'>H</span>
+          <span className='span'>i</span>
+          <span className='span'> </span>
+          <span className='span'>T</span>
+          <span className='span'>h</span>
+          <span className='span'>e</span>
+          <span className='span'>r</span>
+          <span className='span'>e</span>
+          <span className='span'>!</span>
+        </div>
+        <h2 className='h2-greet'>Welcome to my portfolio page, my name is Benas and I am a front-end web developer</h2>
+        <div className='line-break'></div>
+      </div>
+    )
+  }
+1.84666
+
