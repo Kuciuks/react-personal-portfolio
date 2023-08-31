753d1400ea50abcff2ef64f15b36183e16ce79db
Benas <benas.sarachovas@gmail.com>
2023-09-01 00:55:28 +0100

    updated the landing page and header resposniveness, took a better approach

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,8 +4,8 @@ export default function LandingPage() {
 
     return (
       <div className='landing-container'>
-        <h1 className='h1-greet'>Hi There!</h1>
-        <h2 className='h2-greet'>Welcome to my portfolio page, my name is Benas and I am a front-end web developer</h2>
+        <h1 className='h1-greet'>Welcome</h1>
+        <h2 className='h2-greet'>Portofolio of a junior front-end developer </h2>
         <div className='line-break'></div>
       </div>
     )
