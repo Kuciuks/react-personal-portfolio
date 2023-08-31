d4ff7ea9e185a3e66f232803d4cb1c7a56c653e6
Benas <benas.sarachovas@gmail.com>
2023-09-01 00:11:13 +0100

    minor changes

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,17 +4,7 @@ export default function LandingPage() {
 
     return (
       <div className='landing-container'>
-        <div className='welcome-div'>
-          <span className='span'>H</span>
-          <span className='span'>i</span>
-          <span className='span'> </span>
-          <span className='span'>T</span>
-          <span className='span'>h</span>
-          <span className='span'>e</span>
-          <span className='span'>r</span>
-          <span className='span'>e</span>
-          <span className='span'>!</span>
-        </div>
+        <h1>Hi There!</h1>
         <h2 className='h2-greet'>Welcome to my portfolio page, my name is Benas and I am a front-end web developer</h2>
         <div className='line-break'></div>
       </div>
